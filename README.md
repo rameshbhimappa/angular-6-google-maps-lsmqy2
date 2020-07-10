87580d9a842b0a71597f13bf81d375d57af35a72
rameshbhimappa <bsframesh7@gmail.com>
2020-07-10 07:13:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-6-google-maps-lsmqy2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-6-google-maps-lsmqy2)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+})
+export class AppComponent {
+  name: string = 'My first AGM project';
+  lat: number = 14.82390022277832;
+  lng: number = 76.22019958496094;
+}
